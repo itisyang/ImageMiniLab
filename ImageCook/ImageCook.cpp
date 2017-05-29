013f4ce22caddb88c68b654610216c21c1f3c22a
itisyang <itisyang@qq.com>
2017-05-29 16:40:05 +0800

    [add]add rotate menu

--- ImageCook/ImageCook.cpp
+++ ImageCook/ImageCook.cpp
@@ -2,32 +2,87 @@
 
 #include <QFileDialog>
 #include <QDebug>
+#include <QPainter>
 
 ImageCook::ImageCook(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
 	
-	ui.labelshow->clear();
+    //旋转菜单
+//    QAction* RotateRight90 = new QAction();
+//    RotateRight90->setText("向右旋转90度");
+//    QAction* RotateLeft90 = new QAction();
+//    RotateLeft90->setText("向左旋转90度");
+//    QMenu *pRotateMenu = new QMenu();
+//    pRotateMenu->addAction(RotateRight90);
+//    pRotateMenu->addAction(RotateLeft90);
+//    connect(pRotateMenu, SIGNAL(triggered(QAction*)), this, SLOT(on_ActRotate(QAction*)));
+//    ui.BtnRotate->setMenu(pRotateMenu);
+
+    ui.LabShow->clear();
 }
 
-void ImageCook::on_actionopen_triggered()
+void ImageCook::on_ActOpen_triggered()
 {
 	qDebug() << "on_actionopen_triggered";
-	QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"),
-		"/home",
+    m_ImagePath = QFileDialog::getOpenFileName(this, tr("Open File"),
+        "/home",
 		tr("Images (*.png *.xpm *.jpg)"));
-	if (!fileName.isEmpty())
+    if (!m_ImagePath.isEmpty())
 	{
-		QPixmap pix;
-		pix.load(fileName);
-		ui.labelshow->setPixmap(pix);
+        m_Pix.load(m_ImagePath);
+        ui.LabShow->setPixmap(m_Pix);
 	}
 }
 
-void ImageCook::on_actionclose_triggered()
+void ImageCook::on_ActClose_triggered()
 {
 	qDebug() << "on_actionclose_triggered";
-	ui.labelshow->clear();
+    ui.LabShow->clear();
+    m_ImagePath.clear();
+}
+
+void ImageCook::on_ActSave_triggered()
+{
+    if (m_ImagePath.isEmpty())
+    {
+        ui.StatusBar->showMessage("未打开文件", 3000);
+
+        return;
+    }
+
+
+}
+
+void ImageCook::on_ActSaveAs_triggered()
+{
+    if (m_ImagePath.isEmpty())
+    {
+        ui.StatusBar->showMessage("未打开文件", 3000);
+
+        return;
+    }
+}
+
+void ImageCook::on_ActRotate(QAction *act)
+{
+    qDebug() << act->text();
+//    if (act->text() == "向左旋转90度")
+//    {
+//        QPainter painter(&m_Pix);
+//        QPixmap pix = m_Pix;
+//        painter.rotate(90);
+//        painter.drawPixmap(0, 0, pix);
+//        ui.LabShow->clear();
+//        ui.LabShow->setPixmap(m_Pix);
+
+
+//    }
+//    else if (act->text() == "向右旋转90度")
+//    {
+
+//    }
+
 }
 
--- ImageCook/ImageCook.h
+++ ImageCook/ImageCook.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QtWidgets/QMainWindow>
+
 #include "ui_ImageCook.h"
 
 class ImageCook : public QMainWindow
@@ -11,9 +12,14 @@ public:
 	ImageCook(QWidget *parent = Q_NULLPTR);
 
 private slots:
-    void on_actionopen_triggered();
-	void on_actionclose_triggered();
-
+    void on_ActOpen_triggered();
+    void on_ActClose_triggered();
+    void on_ActSave_triggered();
+    void on_ActSaveAs_triggered();
+    void on_ActRotate(QAction *act);
 private:
 	Ui::ImageCookClass ui;
+
+    QPixmap m_Pix;
+    QString m_ImagePath;
 };
